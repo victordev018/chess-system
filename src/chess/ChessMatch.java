e4cb52c52de205cca6d450ef4d1db38629e0b6cc
João VIctor <joawrt8520@gmail.com>
2024-06-18 22:57:38 -0300

    feat: little improviment in board printing

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -38,8 +38,19 @@ public class ChessMatch {
 
     // método para fazer a configuração inicial do tebauleiro
     private void initialSetup(){
-        placeNewPiece('f', 8,new King(board, Color.BLACK));
         placeNewPiece('c', 1, new Rook(board, Color.WHITE));
+        placeNewPiece('c', 2, new Rook(board, Color.WHITE));
+        placeNewPiece('d', 2, new Rook(board, Color.WHITE));
+        placeNewPiece('e', 2, new Rook(board, Color.WHITE));
+        placeNewPiece('e', 1, new Rook(board, Color.WHITE));
+        placeNewPiece('d', 1, new King(board, Color.WHITE));
+
+        placeNewPiece('c', 7, new Rook(board, Color.BLACK));
+        placeNewPiece('c', 8, new Rook(board, Color.BLACK));
+        placeNewPiece('d', 7, new Rook(board, Color.BLACK));
+        placeNewPiece('e', 7, new Rook(board, Color.BLACK));
+        placeNewPiece('e', 8, new Rook(board, Color.BLACK));
+        placeNewPiece('d', 8, new King(board, Color.BLACK));
     }
 
 }
